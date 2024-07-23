7d49e93025313a586908f2af1885f524ed7fca48
tharinduWageesha <tharinduwageesha@gmail.com>
2024-07-23 20:10:52 +0530

    update printbtn

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-document.getElementById
+//Printing Tables-------------------------------------------------------
+function printTable(){
+   
+    var divToPrint = document.getElementById("table-container");
+    var newWin = window.open("");
+    newWin.document.write('<html><head><title>Print Table</title>');
+    newWin.document.write('<style>');
+    newWin.document.write('table { width: 100%; border-collapse: collapse; }');
+    newWin.document.write('th, td { border: 1px solid black; padding: 8px; text-align: left; }');
+    newWin.document.write('th { background-color: #f2f2f2; }');
+    newWin.document.write('</style>');
+    newWin.document.write('</head><body>');
+    newWin.document.write(divToPrint.outerHTML);
+    newWin.document.write('</body></html>');
+    newWin.document.close();
+    newWin.print();
+    newWin.close();
+}
